ee2c181b6a47f0dbf358beece69519c01917ce18
Vipul Jain <vipul.today@gmail.com>
2020-04-28 19:45:58 +0530

    Some great modifications

---
+++ Website/app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,jsonify,make_response
+import pandas as pd
+from recommender_module import similarity_recommender
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+data = pd.read_csv("./data/song.csv")
+data.sort_values(by=["title","artist_name"],ascending = False,inplace = True)
+data.sort_values(by=["listen_count"],ascending = False,inplace = True)
+trending_dict = dict(data.iloc[:7,:])
+
+#Recommendation page
+song1_df = pd.read_csv("./data/recommend_songs.csv")
+train_data, test_data = train_test_split(song1_df, test_size = 0.20, random_state=0)
+
+is_model = similarity_recommender()
+is_model.create_s(train_data, 'user_id', 'Song')
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/about')
+def aboutus():
+   return render_template('about.html')
+
+@app.route('/contactus')
+def contactus():
+   return render_template("contactus.html")
+
+@app.route('/trending')
+def trending():
+   return render_template("trending.html",trending_dict=trending_dict)
+
+@app.route('/recommendations')
+def recommendation():
+   result = is_model.similar_items(['U Smile - Justin Bieber']).to_dict()
+   return render_template("recommendation.html",result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
--- Website/conn.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "songs";
-
-$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
